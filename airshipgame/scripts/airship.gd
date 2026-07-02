578ed2b0736fc2cd3bcc0272e7acf3e0851610fa
goldenpumpk1n <goldenpumpkin215@gmail.com>
2026-07-02 15:42:37 -0700

    i killed a family of 4

--- airshipgame/scripts/airship.gd
+++ airshipgame/scripts/airship.gd
@@ -2,7 +2,11 @@ extends Node3D
 
 var player
 
-var upwardspeed = 5.0
+var upwardspeed = 1
+var downwardspeed = -2
+
+
+var velocity: Vector3 = Vector3.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -12,4 +16,9 @@ func _ready() -> void:
 func _process(delta):
 	if player.is_locked:
 		if Input.is_action_pressed("space"):
-			velocity.y = upwardspeed * delta
+			velocity.y = upwardspeed
+			global_position.y += velocity.y * delta
+			
+		if Input.is_action_pressed("ctrl"):
+			velocity.y = downwardspeed
+			global_position.y += velocity.y * delta
