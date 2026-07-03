3224f31707d964140186b968237b194b7370fa37
= <piggyandbobo@icloud.com>
2026-07-03 15:58:26 -0700

    engine mechanics now work, walk up to block in the back and press E to make the ship go faster. Also can be turned off.

--- airshipgame/models/engine.gd
+++ airshipgame/models/engine.gd
@@ -2,28 +2,27 @@ extends Node3D
 
 @onready var isinarea: Area3D = $Area3D
 
-var player_in_range: bool = false
+var player_in_range := false
 var engineon = false
-var airship
+@onready var airship = get_parent()
+var player
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	airship = get_tree().get_first_node_in_group("airship")
+	player = get_tree().get_first_node_in_group("player")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta: float):
 	if player_in_range:
-		if Input.is_action_pressed("e") and not engineon:
-			engineon = true
-			airship.engine = 4
-		elif Input.is_action_pressed("e") and engineon:
-			engineon = false
-			airship.engine = 1
-	
-func _on_body_entered(body):
+		if Input.is_action_pressed("e"):
+			engineon = not engineon
+			airship.engine = 4 if engineon else 1
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		player_in_range = true
 
-func _on_body_exited(body):
+func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		player_in_range = false
