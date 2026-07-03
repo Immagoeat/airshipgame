80ce598c88ed3fb1c31cb4125a08f30f8ca0ff3a
goldenpumpk1n <goldenpumpkin215@gmail.com>
2026-07-02 20:08:06 -0700

    ief2vnhulrtevfgcluiyhe2frgcvlirf2euh1o24r;euhcvg2;rfieuvch2rf;iuycvh4f2edljkhcg2e4rf['iuycgrf2e;juihcvg4r;iu'fdg3e4rf'yifgh4r3ufgh4r3yui;fghd;i2er4hfvb4r3;fdy12t4rbjfdr3e4hg;foi14gr3uhyfjn4r3efd

--- airshipgame/scripts/airship.gd
+++ airshipgame/scripts/airship.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 @onready var istouching: Area3D = $Istouchingground
+@onready var onship: Area3D = $onship
 
 var player
+var attached_player: CharacterBody3D = null
 
 var isonground := false
 var isdownlocked := false
@@ -10,7 +12,9 @@ var isdownlocked := false
 var upwardspeed = 1
 var downwardspeed = -2
 var movementspeed = 1
-var furnace = 2
+var engine = 2
+var isonship = false
+var total_movement = 0
 
 
 var velocity: Vector3 = Vector3.ZERO
@@ -35,10 +39,40 @@ func _process(delta):
 			
 		if Input.is_action_pressed("up"):
 			velocity.x = movementspeed
-			global_position.x += velocity.x * furnace * delta
+			total_movement = velocity.x * engine * delta
+			global_position.x += total_movement
+			if isonship:
+				player.global_position.x += total_movement
 
 func lockdownwards():
 	isdownlocked = true
 	
 func unlockdownwards():
 	isdownlocked = false
+
+func _on_onship_body_entered(body):
+	if body.is_in_group("player"):
+		isonship = true
+		attached_player = body
+		var original_transform = attached_player.global_transform
+		
+		attached_player.get_parent().remove_child(attached_player)
+		add_child(attached_player)
+		
+		attached_player.global_transform = original_transform
+
+func _on_onship_body_exited(body):
+	if body.is_in_group("player"):
+		isonship = false
+
+var max_speed: float = 10.0
+var acceleration: float = 1.0
+
+var current_speed: float = 0.0
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("right"):
+		if player.is_locked:
+			current_speed += acceleration * delta
+			current_speed = minf(current_speed, max_speed)
+			rotate_y(current_speed * delta)
