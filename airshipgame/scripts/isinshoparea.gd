53dbdd5d044eed8d148a985a8256bf00886d126c
= <piggyandbobo@icloud.com>
2026-07-03 20:55:07 -0700

    added a warp to shopui and fixed up the sky

--- airshipgame/scripts/isinshoparea.gd
+++ airshipgame/scripts/isinshoparea.gd
@@ -1,11 +1,19 @@
 extends Area3D
 
+var player
+var player_in_range := false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player = get_tree().get_first_node_in_group("player")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player_in_range and Input.is_action_pressed("e"):
+		get_tree().change_scene_to_file("res://scenes/shopui.tscn")
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = true
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = false
