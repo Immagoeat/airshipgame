4d2aa48ae46ef7fd50e783d5d60c4d34404ba538
= <piggyandbobo@icloud.com>
2026-07-03 19:32:13 -0700

    working on a shop system, also fixed day/night cycle so it is mostly done.

--- airshipgame/scripts/daycycle.gd
+++ airshipgame/scripts/daycycle.gd
@@ -2,16 +2,33 @@ extends WorldEnvironment
 
 @onready var sun: DirectionalLight3D = $DirectionalLight3D
 
-var daylength: float = 15 # make heigher number later, this is just 
+var daylength: float = 30 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var day_sky_color = Color("#a1d1ff", 0.5)      
+var day_horizon_color = Color("#e8ff96", 0.5)  
+var night_sky_color = Color("#08000f", 0.5)    
+var night_horizon_color = Color("#13001a", 0.5)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	Globals.timeofday = fmod(Globals.timeofday + delta / daylength, 1)
 	sun.rotation_degrees.x = Globals.timeofday * 360 - 90
-	var sunheight: float = -sun.global_transform.basis.z.y
-	sun.light_energy = clampf(sunheight * 4, 0, 2)
+	
+	# Extract the sun's forward direction vector
+	var sun_direction: Vector3 = -sun.global_transform.basis.z
+	
+	var sky_mat = environment.sky.sky_material as ShaderMaterial
+	if sky_mat:
+		# Send the direction and colors to the shader
+		sky_mat.set_shader_parameter("sun_direction", sun_direction)
+		sky_mat.set_shader_parameter("day_sky", day_sky_color)
+		sky_mat.set_shader_parameter("day_horizon", day_horizon_color)
+		sky_mat.set_shader_parameter("night_sky", night_sky_color)
+		sky_mat.set_shader_parameter("night_horizon", night_horizon_color)
+
+	# Smoothly fade light intensity as the sun sets
+	if sun_direction.y > 0:
+		sun.light_energy = clampf(sun_direction.y * 4, 0, 2)
+		sun.visible = true
+	else:
+		sun.light_energy = 0.0
+		sun.visible = false
